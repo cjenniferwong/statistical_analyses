fbdd7c7212d27cb4bcc2558edbf8d0f8c129699f
Jenn Wong <cjenniferwong@gmail.com>
2020-04-10 19:25:56 -0700

    cleaned up code

--- app.py
+++ app.py
@@ -1,32 +1,15 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+#
 from power import power
-
-# importing custom colors
-from colors import *
+#
+# # importing custom colors
+# from colors import *
 
 pages = ['Home', 'Power']
-
-
-@st.cache
-def generate_data(n_columns=10, n_rows=10000):
-    data = []
-    letter_list = ['a', 'b', 'c', 'd', 'e']
-    options = ['letters', 'numbers']
-    for _ in range(n_columns):
-        type = np.random.choice(options)
-        if type == 'letters':
-            data.append(np.random.randint(0, 100, n_rows))
-        else:
-            data.append(np.random.choice(letter_list, n_rows))
-    df = pd.DataFrame(data).T  # this seems to be faster than vstack
-    df.columns = [f'column_{num}' for num in range(len(df.columns))]
-    print('generating data again')
-    return df
 
 
 def main():
